394af43c7e69d5383f97461ae45f0d05b52d3a62
Bhuvaneshwari D <134070992+bhuvi-d@users.noreply.github.com>
2026-05-21 13:32:17 +0530

    Q24

---
+++ Q24.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+         boolean pop = false;
+        HashMap<Character, Character> map = new HashMap<>();
+        if (s.length() != t.length()) {
+            return false;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                if (map.get(s.charAt(i)) == t.charAt(i)) {
+                    pop = true;
+                } else {
+                    return false;
+                }
+            } else {
+                if (map.containsValue(t.charAt(i))) {
+
+                    return false;
+                } else {
+                    map.put(s.charAt(i), t.charAt(i));
+                    pop = true;
+
+                }
+
+
+            }
+        }
+         return pop;
+    
+}
+}
